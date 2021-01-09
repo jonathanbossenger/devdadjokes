db505eadb9b68b3d6396e312fce2a20dba0ec63e
Jonathan Bossenger <jonathanbossenger@gmail.com>
2021-01-09 22:02:29 +0200

    Deploy update
    Updating deploy.template.yaml with app_key
    Updating title on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -398,7 +398,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>Developer Dad Jokes v1.0</h1>
+                    <h1>Dev Dad Jokes v1.0</h1>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
