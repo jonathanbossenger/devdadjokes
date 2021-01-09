77a9b9d2f2761b17d4843acc5236df10ea137c82
Jonathan Bossenger <jonathanbossenger@gmail.com>
2021-01-09 22:16:18 +0200

    Minor updates
    Cleaning up the web route
    Updating the home page view

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,7 @@ Route::get('/', function () {
     $jokeApiRequest = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart';
     $client = new GuzzleHttp\Client();
     $response = $client->get($jokeApiRequest);
-    /**
-     * {#292 ▼
-     * +"error": false
-     * +"category": "Programming"
-     * +"type": "twopart"
-     * +"setup": "What is the best prefix for global variables?"
-     * +"delivery": "//"
-     * +"flags": {#290 ▶}
-     * +"id": 32
-     * +"safe": true
-     * +"lang": "en"
-     * }
-     */
-    $joke = json_decode($response->getBody(), true);
-
+    $joke = json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
     if (isset($joke['error']) && $joke['error'] !== true) {
         $viewData = compact('joke');
     } else {
