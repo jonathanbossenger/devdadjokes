8e252adf02f77402cb3573603d2165651a9c8805
Jonathan Bossenger <jonathanbossenger@gmail.com>
2021-02-15 21:19:30 +0200

    Adding Tailwind support
    Followed details here https://tailwindcss.com/docs/guides/laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <title>Dev Dad Jokes</title>
 
         <!-- Fonts -->
