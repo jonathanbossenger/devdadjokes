546783494323de4a15014997ca219b8c6e36b784
Jonathan Bossenger <jonathanbossenger@gmail.com>
2021-02-28 21:28:34 +0200

    Fixing production issues
    Purge correct files
    Fix https routing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,33 @@
 module.exports = {
-  purge: {
-    content: ['./public/**/*.html'],
-    options: {
-      safelist: []
-    }
-  },
-  theme: {
-    extend: {
-      fontFamily: {
-        "body": ["Consolas", "monaco", "monospace"]
-      },
-      colors: {
-        black: {
-          "400": "#373737",
-          "600": "#404040",
-          "700": "#414141",
-          "900": "#212121"
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                "body": ["Consolas", "monaco", "monospace"]
+            },
+            colors: {
+                black: {
+                    "400": "#373737",
+                    "600": "#404040",
+                    "700": "#414141",
+                    "900": "#212121"
+                },
+                "win-close": "#E51323",
+                "win-maximize": "#E8E8E8",
+                "win-minimize": "#E8E8E8",
+                "mac-close": "#F35F57",
+                "mac-maximize": "#F8BC2E",
+                "mac-minimize": "#44C83F"
+            },
+            minHeight: {
+                '64': '16rem'
+            }
         },
-        "win-close": "#E51323",
-        "win-maximize": "#E8E8E8",
-        "win-minimize": "#E8E8E8",
-        "mac-close": "#F35F57",
-        "mac-maximize": "#F8BC2E",
-        "mac-minimize": "#44C83F"
-      },
-      minHeight: {
-        '64': '16rem'
-       }
     },
-  },
-  variants: {},
-  plugins: [],}
+    variants: {},
+    plugins: [],
+}
